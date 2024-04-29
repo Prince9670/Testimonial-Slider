c4d6bffdb74b5c9e039af898378beaa4e3896b63
Prince Kaushal <95558284+Prince9670@users.noreply.github.com>
2024-04-30 00:01:29 +0530

    first commit

---
+++ src/components/data.js
@@ -0,0 +1,54 @@
+const reviews = [
+    {
+      id: 1,
+      name: "Prince Kaushal",
+      job: "Software Engineer",
+      image: "https://i.ibb.co/NK4RtCJ/Whats-App-Image-2023-12-29-at-1-22-03-AM.jpg",
+      text: "Working with this team was a pleasure. Their technical expertise and ability to deliver quality solutions on time and within budget were impressive. I highly recommend them as a true partner and asset to any project.",
+    },
+    {
+      id: 2,
+      name: "Rishabh Gupta",
+      job: "Android Developer",
+      image: "https://i.ibb.co/VQMyYK2/Whats-App-Image-2023-12-29-at-1-21-05-AM.jpg",
+      text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+    },
+    {
+      id: 3,
+      name: "Vishal Singh",
+      job: "Frontend Developer",
+      image: "https://i.ibb.co/ccQvwfY/Whats-App-Image-2023-12-29-at-1-21-05-AM-1.jpg",
+      text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+    },
+    {
+      id: 4,
+      name: "Sarthak Srivastava",
+      job: "Web Developer",
+      image: "https://i.ibb.co/G5JwCSg/Whats-App-Image-2023-12-29-at-1-21-05-AM-2.jpg",
+      text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+    },
+    {
+      id: 5,
+      name: "Ridhima Saxena",
+      job: "Software Engineer",
+      image: "https://i.ibb.co/brRbjGP/Whats-App-Image-2023-12-29-at-1-21-06-AM.jpg",
+      text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+    },
+    {
+      id: 6,
+      name: "Shivam Kumar",
+      job: "Data Analyst",
+      image: "https://i.ibb.co/02FfYX1/Whats-App-Image-2023-12-29-at-1-34-20-AM.jpg",
+      text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+    },
+
+    {
+        id: 7,
+        name: "Vidyadhar Chaudhary",
+        job: "Graphic Designer",
+        image: "https://i.ibb.co/9HpnN9L/Whats-App-Image-2023-12-29-at-1-21-06-AM-2.jpg",
+        text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+      },
+  ];
+  
+  export default reviews;
